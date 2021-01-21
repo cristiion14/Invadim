cf1911fc5547fe5f2251a7b440e0cfa44a083fed
cristiion14 <cristi.ion1996@gmail.com>
2021-01-21 14:12:14 +0200

    Added Project version 2019.4.17f1

---
+++ Unity Project/Backups/Unity 2019.4.17f1/Invadim/Assets/Scripts/TagsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagsManager 
+{
+    public const string scuipat = "Scuipa";
+    public const string bullet = "Bullet";
+    public const string gaina = "Gaina";
+    public const string vadim = "Vadim";
+
+    //Audio sounds
+    public const string GameSound = "Game Sound";
+    public const string ScuipatSound = "Scuipat";
+}
+
